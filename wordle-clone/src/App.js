297176f3750c46bc24d6ef18141cc0b26b23e6ea
Hunter Andersen <andersen.hunter42@gmail.com>
2022-02-23 17:32:38 -0800

    clearing some console logs

--- wordle-clone/src/App.js
+++ wordle-clone/src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 import TileRow from './Components/TileRow';
 
 function App() {
+
+  const tileRowArray = [];
+
   return (
-    <div className="App">
+    <div className="App" onFocus={() =>{
+      let newFocusTarget = querySelector();
+    }}>
       <h1 className="title" >Wordle Clone</h1>
       <TileRow/>
     </div>
