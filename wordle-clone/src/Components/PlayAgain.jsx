ecb796a970799ed42aec2e8b96fe9dd39d10a132
Hunter Andersen <andersen.hunter42@gmail.com>
2022-07-17 19:02:05 -0700

    fix:PlayAgain button border and text color match correctly

--- wordle-clone/src/Components/PlayAgain.jsx
+++ wordle-clone/src/Components/PlayAgain.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { MESSAGE_WIN } from "../resources/MyConstants";
 
 export default function PlayAgain({ handlePress, statusMessage }) {
+  let statusColor = statusMessage===MESSAGE_WIN ? "statusColorGreen" : "statusColorRed";
+  let borderColor = statusMessage===MESSAGE_WIN ? "playAgainBorderGreen" : "playAgainBorderRed";
   return (
-    // <button className="playAgain" onClick={handlePress}>
-    <button className="playAgain" onClick={handlePress}>
-      <h2 className="statusColorGreen">{statusMessage}</h2>
+    <button className={`playAgain ${borderColor}`} onClick={handlePress}>
+      <h2 className={statusColor}>{statusMessage}</h2>
       <h2 className="titleColor">Play Again</h2>
     </button>
   )
